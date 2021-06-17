0dc489e3caced2e4bbf5bc695e40fceda1d4d79c
Saidumarov Semyen <74598420+s3m3n1s@users.noreply.github.com>
2021-06-17 23:52:33 +0300

    Fixed turn off/turn on wuth server api

--- client.py
+++ client.py
@@ -5,6 +5,8 @@ from config import port
 protocol = "http://"
 
 
+br_before_off = 50
+
 def get_lamps_list():
     l_list = requests.get(protocol + ip + ":" + str(port) + "/api/lamps")
     return l_list.json()
@@ -21,10 +23,10 @@ def brightness_up(name, n):
     for i in get_lamps_list():
         if i['name'] == name:
             if i['brightness'] + n >= 100:
-                update_lamp(i['id'], i['name'],  i['power'], 100)
+                update_lamp(i['id'], i['name'], i['power'], 100)
                 return 100
             else:
-                update_lamp(i['id'], i['name'], i['power'], i['brightness'] + n )
+                update_lamp(i['id'], i['name'], i['power'], i['brightness'] + n)
                 return i['brightness'] + n
 
 
@@ -32,23 +34,27 @@ def brightness_down(name, n):
     for i in get_lamps_list():
         if i['name'] == name:
             if i['brightness'] - n <= 0:
-                update_lamp(i['id'], i['name'],  i['power'], 0)
+                update_lamp(i['id'], i['name'], i['power'], 0)
                 return 0
             else:
-                update_lamp(i['id'], i['name'],  i['power'], i['brightness'] - n,)
+                update_lamp(i['id'], i['name'], i['power'], i['brightness'] - n, )
                 return i['brightness'] - n
 
 
 def turn(name):
+    global br_before_off
     for i in get_lamps_list():
         if i['name'] == name:
-            update_lamp(i['id'], i['name'],  not i['power'],i['brightness'],)
+            if i['brightness']:
+                br_before_off = i['brightness']
+                update_lamp(i['id'], i['name'],  i['power'], 0)
+            else:
+                update_lamp(i['id'], i['name'], i['power'], br_before_off)
 
 
 def is_power(name):
     for i in get_lamps_list():
         if i['name'] == name:
-            if i['power']:
+            if i['brightness'] != 0:
                 return True
     return False
-
